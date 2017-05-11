685f85737d691f0a9963ce143d4693b262b62836
Mario Arduini <mario.arduini.gen@gmail.com>
2017-05-11 21:09:26 +0800

    big refactoring, created expand node function

--- week2/driver_3.py
+++ week2/driver_3.py
@@ -24,6 +24,39 @@ if DEBUG_VAR :
         print(board[3:6])
         print(board[6:9])
 
+# Utility to expand nodes (require node and direction [0 for 'Up',1 for 'Right',2 for 'Down',3 for 'Left'])
+def expnode(node,dir,i):
+    global msd
+    global explored
+    global fronted
+    global front
+    if dir is 0:
+        dirword = 'Up'
+        offset = -3
+    elif dir is 1:
+        dirword = 'Right'
+        offset = 1
+    elif dir is 2:
+        dirword = 'Down'
+        offset = 3
+    elif dir is 3:
+        dirword = 'Left'
+        offset = -1
+    else:
+        if DEBUG_VAR : print("Error: in expnode not valid direction")
+        return
+    newboard = node.board.copy()
+    newboard[i+offset],newboard[i] = newboard[i],newboard[i+offset]
+    new = State(newboard,node.layer+1,node,dirword)
+    if not (new.value in explored or new.value in fronted):
+        if DEBUG_VAR :
+            print('new node generated from '+dirword+' (depth '+str(new.layer)+' ) move: ')
+            pb(new.board)
+        if new.layer > msd : msd = new.layer
+        fronted[new.value]=new
+        front.append(new)
+
+
 # Utility to generate the output file (require a tuple with final state, explored nodes and maximum depth search)
 def outgen(tuple):
     f = open('output.txt','w')
@@ -51,9 +84,10 @@ def outgen(tuple):
 
 # implementation of Breadth First Search
 def bfs():
+    global front
     front = deque([inits])
-    exp=0
-    msd=0
+    global exp
+    global msd
     while len(front)>0:
         cur = front.popleft()
 
@@ -72,60 +106,25 @@ def bfs():
             if cur.board[i] == '0':
                 if i>2:
                     #expand up
-                    newboard = cur.board.copy()
-                    newboard[i-3],newboard[i] = newboard[i],newboard[i-3]
-                    new = State(newboard,cur.layer+1,cur,'Up')
-                    if not (new.value in explored or new.value in fronted):
-                        if DEBUG_VAR :
-                            print('new node generated from "up" (depth '+str(new.layer)+' ) move: ')
-                            pb(new.board)
-                        if new.layer > msd : msd = new.layer
-                        fronted[new.value]=new
-                        front.append(new)
+                    expnode(cur,0,i)
                 if i<6:
                     #expand down
-                    newboard = cur.board.copy()
-                    newboard[i+3],newboard[i] = newboard[i],newboard[i+3]
-                    new = State(newboard,cur.layer+1,cur,'Down')
-                    if not (new.value in explored or new.value in fronted):
-                        if DEBUG_VAR :
-                            print('new node generated from "down" (depth '+str(new.layer)+' ) move: ')
-                            pb(new.board)
-                        if new.layer > msd : msd = new.layer
-                        fronted[new.value]=new
-                        front.append(new)
+                    expnode(cur,2,i)
                 if i in range(1,3) or i in range(4,6) or i in range(7,9):
                     #expand left
-                    newboard = cur.board.copy()
-                    newboard[i-1],newboard[i] = newboard[i],newboard[i-1]
-                    new = State(newboard,cur.layer+1,cur,'Left')
-                    if not (new.value in explored or new.value in fronted):
-                        if DEBUG_VAR :
-                            print('new node generated from "left" (depth '+str(new.layer)+' ) move: ')
-                            pb(new.board)
-                        if new.layer > msd : msd = new.layer
-                        fronted[new.value]=new
-                        front.append(new)
+                    expnode(cur,3,i)
                 if i in range(0,2) or i in range(3,5) or i in range(6,8):
                     #expand right
-                    newboard = cur.board.copy()
-                    newboard[i+1],newboard[i] = newboard[i],newboard[i+1]
-                    new = State(newboard,cur.layer+1,cur,'Right')
-                    if not (new.value in explored or new.value in fronted):
-                        if DEBUG_VAR :
-                            print('new node generated from "right" (depth '+str(new.layer)+' ) move: ')
-                            pb(new.board)
-                        if new.layer > msd : msd = new.layer
-                        fronted[new.value]=new
-                        front.append(new)
+                    expnode(cur,1,i)
                 break
     return None
 
 # Depth First Search
 def dfs():
+    global front
     front = [inits]
-    exp = 0
-    msd = 0
+    global exp
+    global msd
     while len(front)>0:
         cur = front.pop();
 
@@ -144,52 +143,16 @@ def dfs():
             if cur.board[i] == '0':
                 if i in range(0,2) or i in range(3,5) or i in range(6,8):
                     #expand right
-                    newboard = cur.board.copy()
-                    newboard[i+1],newboard[i] = newboard[i],newboard[i+1]
-                    new = State(newboard,cur.layer+1,cur,'Right')
-                    if not (new.value in explored or new.value in fronted):
-                        if DEBUG_VAR :
-                            print('new node generated from "right" (depth '+str(new.layer)+' ) move: ')
-                            pb(new.board)
-                        if new.layer > msd : msd = new.layer
-                        fronted[new.value]=new
-                        front.append(new)
+                    expnode(cur,1,i)
                 if i in range(1,3) or i in range(4,6) or i in range(7,9):
                     #expand left
-                    newboard = cur.board.copy()
-                    newboard[i-1],newboard[i] = newboard[i],newboard[i-1]
-                    new = State(newboard,cur.layer+1,cur,'Left')
-                    if not (new.value in explored or new.value in fronted):
-                        if DEBUG_VAR :
-                            print('new node generated from "left" (depth '+str(new.layer)+' ) move: ')
-                            pb(new.board)
-                        if new.layer > msd : msd = new.layer
-                        fronted[new.value]=new
-                        front.append(new)
+                    expnode(cur,3,i)
                 if i<6:
                     #expand down
-                    newboard = cur.board.copy()
-                    newboard[i+3],newboard[i] = newboard[i],newboard[i+3]
-                    new = State(newboard,cur.layer+1,cur,'Down')
-                    if not (new.value in explored or new.value in fronted):
-                        if DEBUG_VAR :
-                            print('new node generated from "down" (depth '+str(new.layer)+' ) move: ')
-                            pb(new.board)
-                        if new.layer > msd : msd = new.layer
-                        fronted[new.value]=new
-                        front.append(new)
+                    expnode(cur,2,i)
                 if i>2:
                     #expand up
-                    newboard = cur.board.copy()
-                    newboard[i-3],newboard[i] = newboard[i],newboard[i-3]
-                    new = State(newboard,cur.layer+1,cur,'Up')
-                    if not (new.value in explored or new.value in fronted):
-                        if DEBUG_VAR :
-                            print('new node generated from "up" (depth '+str(new.layer)+' ) move: ')
-                            pb(new.board)
-                        if new.layer > msd : msd = new.layer
-                        fronted[new.value]=new
-                        front.append(new)
+                    expnode(cur,0,i)
                 break
     return None
 
@@ -204,8 +167,11 @@ inits = State(sys.argv[2].split(','),0,None,'')
 solved = 0
 for i in range(1,9):
     solved += i*i
-explored = {}
-fronted = {inits.value : inits}
+explored = {} # dictionary containing explored nodes
+fronted = {inits.value : inits} # dictionary containing frontier nodes
+front = [] # frontier
+exp = 0 # number of explored nodes
+msd = 0 # maximum search depth
 if sys.argv[1] == 'bfs':
     if DEBUG_VAR : print('bfs calling:')
     solution = bfs()
