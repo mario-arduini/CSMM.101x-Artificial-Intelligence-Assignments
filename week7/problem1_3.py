b35dc1d0aff6af4224260b4ffe57bd3f5e554bf0
Mario Arduini <mario.arduini.gen@gmail.com>
2017-06-23 12:02:50 +0800

    implemented perceptron algorithm and plotting results, documented

--- week7/problem1_3.py
+++ week7/problem1_3.py
@@ -1,11 +1,40 @@
 import pandas as pd
+import numpy as np
+import sys
 
+def main():
+    # read data from input, expected csv, 2 feature and 1 label
+    fin = sys.argv[1]
+    dset = pd.read_csv(fin,header=None)
+    data = np.array([np.ones(len(dset)),dset[0],dset[1]]).T
+    yi = np.array(dset[2])
 
+    # prepare output
+    #fout = sys.argv[2]
 
-def main:
-    dset = pd.read_csv("input1")
-    print(dset)
+    # initialize weights
+    w = np.ones(3)
+    wold = np.array([-1,-1,-1])
 
+    # perceptron algorithm
+    while not (w==wold).all() :
+        wold = w.copy()
+        for i,xi in enumerate(data):
+            if np.sum(xi*w)*yi[i] < 0 :
+                w += xi*yi[i]
+                print(w)
+
+    # save results
+    print(w)
+
+    # plot if required
+    if len(sys.argv>3) and sys.argv[3] is '-plot':
+        import matplotlib.pyplot as plt
+        plt.plot(dset[0][yi==1],dset[1][yi==1],"or")
+        plt.plot(dset[0][yi==-1],dset[1][yi==-1],"ob")
+        xval = np.arange(15)
+        plt.plot(xval,-w[1]*xval/w[2]-w[0]/w[2])
+        plt.show()
 
 
 if __name__ == "__main__" :
